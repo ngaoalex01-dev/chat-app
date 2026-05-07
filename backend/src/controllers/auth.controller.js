3785de9718a946f5e2d7a9ff50c3dc4f430b46be
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-05-08 00:15:00 +0300

    loginn

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -106,6 +106,11 @@ export const verifyEmail = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password} = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  
   try {
     const user = await User.findOne({ email});
     if(!user) return res.status(400).json({ message: "Invalid credentials" });
