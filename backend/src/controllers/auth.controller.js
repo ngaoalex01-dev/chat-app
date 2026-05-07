774ae0b69665693acac196d33ac59cb179c7c3d6
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-05-07 23:27:45 +0300

    login-logout functionalities added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -105,9 +105,29 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("Login endpoint");
+  const { email, password} = req.body;
+  try {
+    const user = await User.findOne({ email});
+    if(!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+
+     generateToken(user._id, res);
+     res.status(200).json ({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePicture: user.profilePicture,
+     });
+  } catch (error) {
+    console.error("error in login controller:", error);
+    res.status(500).json({message: "internal server error" });
+  }
 };
 
-export const logout = (req, res) => {
-  res.send("Logout endpoint");
+export const logout = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
 };
