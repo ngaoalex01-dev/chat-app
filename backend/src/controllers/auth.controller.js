227ffcfc0bb9925afb527f02f86deb807d1c422f
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-05-08 16:15:12 +0300

    authcontroler...hide password

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -147,7 +147,7 @@ export const updateProfile = async (req, res) => {
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
 
-   const updatedUser =  await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
+   const updatedUser =  await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true }).select("-password");
 
    res.status(200).json({updatedUser});
 
@@ -155,6 +155,6 @@ export const updateProfile = async (req, res) => {
 
     console.log("Error in update profile:", error);
     res.status(500).json({ message: "Internal server error" });
-    
+
   }
 };
