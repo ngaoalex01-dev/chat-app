11fd8e81c2c989de775a7ae0aaf7818b656c41bc
ngaoalex01-dev <ngaoalex01@gmail.com>
2026-05-29 00:32:20 +0300

    fix: resolve 413 payload error for image uploads

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,8 @@ const __dirname = path.resolve();
 
 const PORT = ENV.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors({ origin: ENV.clientURL, credentials: true }));
 app.use(cookieParser());
 
